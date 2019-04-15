7cc782bc5f7059e71cfcbda17f15cc7c0ab7674c
AndreiOvidiuCiobanu <an19drei@gmail.com>
2019-04-15 09:30:49 +0300

    First commit

---
+++ app/src/main/java/com/example/ioana/travel_journal/Login.java
@@ -0,0 +1,134 @@
+package com.example.ioana.travel_journal;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class Login extends AppCompatActivity {
+
+    private Button mButtonSignIn;
+    private static final int RC_SIGN_IN = 200;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+    static final String TAG = "Login";
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initView();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null){
+                    Log.w( null, "onAuthStateChanged-BAAAAAAA");
+                    Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    String email = user.getEmail();
+                    String nume = user.getDisplayName();
+                    Uri image = user.getPhotoUrl();
+                    intent.putExtra("imageUri",image);
+                    intent.putExtra("email",email);
+                    intent.putExtra("nume",nume);
+                    startActivity(intent);
+                }
+            }
+        };
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mButtonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void initView(){
+        mButtonSignIn = findViewById(R.id.button_signin);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.w( null, "REQEST CODE PUTEE-BAAAAAAA");
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Log.w( null, "RC_SiGN_IN-BAAAAAAA");
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+                Log.w( null, "IA PULAAA-BAAAAAAA");
+                startActivity(new Intent(Login.this, MenuActivity.class));
+
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
+    }
+
+}
---
+++ app/src/main/java/com/example/ioana/travel_journal/TripViewHolder.java
@@ -0,0 +1,33 @@
+package com.example.ioana.travel_journal;
+
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TripViewHolder extends RecyclerView.ViewHolder {
+    public ImageView mImageViewPicture;
+    public TextView mTextViewName;
+    public TextView mTextViewDestination;
+    public TextView mTextViewRating;
+    public CheckBox mCheckBoxFavourite;
+
+
+    public TripViewHolder(@NonNull View itemView) {
+        super(itemView);
+        mImageViewPicture = itemView.findViewById(R.id.imageview_picture);
+        mTextViewName = itemView.findViewById(R.id.textview_type);
+        mTextViewDestination = itemView.findViewById(R.id.textview_type);
+        mTextViewRating = itemView.findViewById(R.id.textview_rating);
+        mCheckBoxFavourite = itemView.findViewById(R.id.checkbox_bookmark);
+    }
+
+
+
+}
+
+
+
